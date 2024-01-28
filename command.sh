72cc33d5e6ea1570fa42cfcb5a8f620fdd8503f3
Ayat-cmd <ayatram4@gmail.com>
2024-01-28 03:24:43 +0300

    sh

---
+++ command.sh
@@ -0,0 +1,8 @@
+sudo apt update
+sudo apt install python3-pip -y
+pip3 install natasha
+pip3 install pyTelegramBotAPI
+
+git clone https://github.com/Ayat-cmd/bot_extract_text.git
+cd bot_extract_text
+python3 main.py
